61e5ead445e96223bca9fcb3b58211643bf150f4
Kevin Gurney <kgurney@mathworks.com>
2023-09-08 16:20:33 -0400

    Move refactored tabular tests to refactor folder.

--- matlab/test/arrow/tabular/refactor/tRecordBatch.m
+++ matlab/test/arrow/tabular/refactor/tRecordBatch.m
@@ -16,7 +16,7 @@
 % implied.  See the License for the specific language governing
 % permissions and limitations under the License.
 
-classdef tRecordBatchRefactor < hTabular
+classdef tRecordBatch < hTabular
 
     properties
 
--- matlab/test/arrow/tabular/refactor/tTable.m
+++ matlab/test/arrow/tabular/refactor/tTable.m
@@ -16,7 +16,7 @@
 % implied.  See the License for the specific language governing
 % permissions and limitations under the License.
 
-classdef tTableRefactor < hTabular
+classdef tTable < hTabular
 
     properties
 
