123fc39bed534987aa8d35bbc7f60e3220bddc66
Sarah Gilmore <sgilmore@mathworks.com>
2023-11-16 15:22:23 -0500

    Add function to create a release on GitHub

---
+++ dev/release/github/release.sh
@@ -0,0 +1,45 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+release() {
+  if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <release_name> <release_description> [prerelease=yes|no]"
+    exit
+  fi
+
+  release_name=$1
+  release_description=$2
+
+  if ["$#" -ge 3] && ["$3" = "yes"]; then
+      prerelease=true
+  else
+      prerelease=false
+  fi
+
+  gh api \
+    --method POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    /repos/apache/arrow/releases \
+    -f tag_name="${release_name}" \
+    -f target_commitish='main' \
+    -f name=${release_name}" \
+    -f body="${release_description}" \
+    -F draft=false \
+    -F prerelease=$prerelease \
+    -F generate_release_notes=false 
+}
